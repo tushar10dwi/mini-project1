2098bec5b5a6b60444350790df01e61976b26ca2
tushar10dwi <tushar10dwi@gmail.com>
2026-08-25 19:55:38 +0530

    Extend exec.c to perform i/o redirections

--- c-shell/src/exec.c
+++ c-shell/src/exec.c
@@ -1,6 +1,6 @@
 #include "exec.h"
-
 #include <errno.h>
+#include <fcntl.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,6 +12,14 @@
 #ifndef PATH_MAX
 #define PATH_MAX 4096
 #endif
+
+#define MAX_REDIR 32
+#define IO_BUF_SIZE 65536
+
+typedef struct {
+    char *filename;
+    int append;
+} output_redir_t;
 
 static int is_executable_file(const char *path)
 {
@@ -91,39 +99,271 @@ static int resolve_executable(const char *raw_name, char *out, size_t out_size, 
     return 0;
 }
 
+static int parse_redirections(int argc, char **argv, char **clean_argv, int *clean_argc, char **input_files, int *n_input, output_redir_t *output_files, int *n_output)
+{
+    *clean_argc = 0;
+    *n_input = 0;
+    *n_output = 0;
+
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "<") == 0) {
+            if (i + 1 >= argc) {
+                fprintf(stderr, "cshell: missing filename after '<'\n");
+                return -1;
+            }
+            if (*n_input >= MAX_REDIR) {
+                fprintf(stderr, "cshell: too many input redirections\n");
+                return -1;
+            }
+            input_files[(*n_input)++] = argv[++i];
+        } else if (strcmp(argv[i], ">>") == 0) {
+            if (i + 1 >= argc) {
+                fprintf(stderr, "cshell: missing filename after '>>'\n");
+                return -1;
+            }
+            if (*n_output >= MAX_REDIR) {
+                fprintf(stderr, "cshell: too many output redirections\n");
+                return -1;
+            }
+            output_files[*n_output].filename = argv[++i];
+            output_files[*n_output].append = 1;
+            (*n_output)++;
+        } else if (strcmp(argv[i], ">") == 0) {
+            if (i + 1 >= argc) {
+                fprintf(stderr, "cshell: missing filename after '>'\n");
+                return -1;
+            }
+            if (*n_output >= MAX_REDIR) {
+                fprintf(stderr, "cshell: too many output redirections\n");
+                return -1;
+            }
+            output_files[*n_output].filename = argv[++i];
+            output_files[*n_output].append = 0;
+            (*n_output)++;
+        } else {
+            clean_argv[(*clean_argc)++] = argv[i];
+        }
+    }
+    clean_argv[*clean_argc] = NULL;
+    return 0;
+}
+
+static int build_concatenated_stdin(int *input_fds, int n_input)
+{
+    FILE *tmp = tmpfile();
+    if (tmp == NULL) {
+        perror("cshell: tmpfile");
+        for (int k = 0; k < n_input; k++) {
+            close(input_fds[k]);
+        }
+        return -1;
+    }
+
+    char buf[IO_BUF_SIZE];
+    for (int k = 0; k < n_input; k++) {
+        ssize_t r;
+        while ((r = read(input_fds[k], buf, sizeof(buf))) > 0) {
+            if (fwrite(buf, 1, (size_t)r, tmp) != (size_t)r) {
+                perror("cshell: write to temp file");
+                close(input_fds[k]);
+                fclose(tmp);
+                return -1;
+            }
+        }
+        close(input_fds[k]);
+    }
+
+    fflush(tmp);
+    rewind(tmp);
+
+    int fd = dup(fileno(tmp));
+    fclose(tmp);
+    return fd;
+}
+
+static pid_t start_output_tee(int *output_fds, int n_output, int *pipe_write_fd)
+{
+    int pipefd[2];
+    if (pipe(pipefd) != 0) {
+        perror("cshell: pipe");
+        return -1;
+    }
+
+    pid_t tee_pid = fork();
+    if (tee_pid < 0) {
+        perror("cshell: fork");
+        close(pipefd[0]);
+        close(pipefd[1]);
+        return -1;
+    }
+
+    if (tee_pid == 0) {
+        /* Tee helper: read the pipe, fan out to every output file. */
+        close(pipefd[1]);
+        char buf[IO_BUF_SIZE];
+        ssize_t r;
+        while ((r = read(pipefd[0], buf, sizeof(buf))) > 0) {
+            for (int k = 0; k < n_output; k++) {
+                ssize_t written = 0;
+                while (written < r) {
+                    ssize_t w = write(output_fds[k], buf + written, (size_t)(r - written));
+                    if (w < 0) {
+                        break;
+                    }
+                    written += w;
+                }
+            }
+        }
+        close(pipefd[0]);
+        for (int k = 0; k < n_output; k++) {
+            close(output_fds[k]);
+        }
+        _exit(0);
+    }
+
+    close(pipefd[0]);
+    *pipe_write_fd = pipefd[1];
+    return tee_pid;
+}
+
 int exec_command(int argc, char **argv)
 {
     if (argc < 1 || argv[0] == NULL || argv[0][0] == '\0') {
         return -1;
     }
 
+    char *clean_argv[argc + 1];
+    int clean_argc = 0;
+
+    char *input_files[MAX_REDIR];
+    int n_input = 0;
+
+    output_redir_t output_files[MAX_REDIR];
+    int n_output = 0;
+
+    if (parse_redirections(argc, argv, clean_argv, &clean_argc, input_files, &n_input, output_files, &n_output) != 0) {
+        return -1;
+    }
+
+    if (clean_argc < 1) {
+        fprintf(stderr, "cshell: no command specified\n");
+        return -1;
+    }
+
     char resolved[PATH_MAX + 256];
     char *bare_name = NULL;
-    int found = resolve_executable(argv[0], resolved, sizeof(resolved), &bare_name);
+    int found = resolve_executable(clean_argv[0], resolved, sizeof(resolved), &bare_name);
 
     if (!found) {
-        printf("cshell: command not found (%s)\n", bare_name != NULL ? bare_name : argv[0]);
+        printf("cshell: command not found (%s)\n", bare_name != NULL ? bare_name : clean_argv[0]);
         free(bare_name);
         return -1;
+    }
+
+    int input_fds[MAX_REDIR];
+    for (int k = 0; k < n_input; k++) {
+        input_fds[k] = open(input_files[k], O_RDONLY);
+        if (input_fds[k] < 0) {
+            printf("cshell: no such file or directory\n");
+            for (int j = 0; j < k; j++) {
+                close(input_fds[j]);
+            }
+            free(bare_name);
+            return -1;
+        }
+    }
+
+    int output_fds[MAX_REDIR];
+    for (int k = 0; k < n_output; k++) {
+        int flags = O_WRONLY | O_CREAT | (output_files[k].append ? O_APPEND : O_TRUNC);
+        output_fds[k] = open(output_files[k].filename, flags, 0644);
+        if (output_fds[k] < 0) {
+            printf("cshell: unable to create file for writing\n");
+            for (int j = 0; j < k; j++) {
+                close(output_fds[j]);
+            }
+            for (int j = 0; j < n_input; j++) {
+                close(input_fds[j]);
+            }
+            free(bare_name);
+            return -1;
+        }
+    }
+
+    int stdin_fd = -1; 
+    if (n_input == 1) {
+        stdin_fd = input_fds[0];
+    } else if (n_input > 1) {
+        stdin_fd = build_concatenated_stdin(input_fds, n_input);
+        if (stdin_fd < 0) {
+            for (int j = 0; j < n_output; j++) {
+                close(output_fds[j]);
+            }
+            free(bare_name);
+            return -1;
+        }
+    }
+
+    int need_tee = (n_output > 1);
+    int pipe_write_fd = -1;
+    pid_t tee_pid = -1;
+
+    if (need_tee) {
+        tee_pid = start_output_tee(output_fds, n_output, &pipe_write_fd);
+        if (tee_pid < 0) {
+            if (stdin_fd != -1) {
+                close(stdin_fd);
+            }
+            for (int j = 0; j < n_output; j++) {
+                close(output_fds[j]);
+            }
+            free(bare_name);
+            return -1;
+        }
     }
 
     pid_t pid = fork();
     if (pid < 0) {
         perror("cshell: fork");
+        if (stdin_fd != -1) {
+            close(stdin_fd);
+        }
+        if (need_tee) {
+            close(pipe_write_fd);
+        }
+        for (int j = 0; j < n_output; j++) {
+            close(output_fds[j]);
+        }
         free(bare_name);
         return -1;
     }
 
     if (pid == 0) {
-        char **child_argv = malloc((size_t)(argc + 1) * sizeof(char *));
+        if (stdin_fd != -1) {
+            dup2(stdin_fd, STDIN_FILENO);
+            close(stdin_fd);
+        }
+
+        if (need_tee) {
+            dup2(pipe_write_fd, STDOUT_FILENO);
+            close(pipe_write_fd);
+        } else if (n_output == 1) {
+            dup2(output_fds[0], STDOUT_FILENO);
+        }
+
+        for (int k = 0; k < n_output; k++) {
+            close(output_fds[k]);
+        }
+
+        char **child_argv = malloc((size_t)(clean_argc + 1) * sizeof(char *));
         if (child_argv == NULL) {
             _exit(127);
         }
         child_argv[0] = bare_name;
-        for (int i = 1; i < argc; i++) {
-            child_argv[i] = argv[i];
+        for (int i = 1; i < clean_argc; i++) {
+            child_argv[i] = clean_argv[i];
         }
-        child_argv[argc] = NULL;
+        child_argv[clean_argc] = NULL;
 
         execv(resolved, child_argv);
 
@@ -131,8 +371,21 @@ int exec_command(int argc, char **argv)
         _exit(127);
     }
 
+    if (stdin_fd != -1) {
+        close(stdin_fd);
+    }
+    if (need_tee) {
+        close(pipe_write_fd);
+    }
+    for (int k = 0; k < n_output; k++) {
+        close(output_fds[k]);
+    }
+
     int status;
     waitpid(pid, &status, 0);
+    if (need_tee) {
+        waitpid(tee_pid, NULL, 0);
+    }
 
     free(bare_name);
     return 0;
